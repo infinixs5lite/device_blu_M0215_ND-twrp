467011d343399379ee5598005c8e14426026094b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-02-08 10:08:02 +0100

    M0215_ND: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: blu Model: M10L Pro Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_M0215_ND.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_M0215_ND-user \
+    omni_M0215_ND-userdebug \
+    omni_M0215_ND-eng
