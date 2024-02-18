5cf8033dab6f429c5cdec0fb02e25cf7368f9da5
infinixs5lite <kelvinchinedu97@gmail.com>
2024-02-18 21:25:00 +0100

    Update and rename omni_M0215_ND.mk to twrp_M0215_ND.mk

--- twrp_M0215_ND.mk
+++ twrp_M0215_ND.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from M0215_ND device
 $(call inherit-product, device/blu/M0215_ND/device.mk)
 
 PRODUCT_DEVICE := M0215_ND
-PRODUCT_NAME := omni_M0215_ND
+PRODUCT_NAME := twrp_M0215_ND
 PRODUCT_BRAND := BLU
 PRODUCT_MODEL := M10L Pro
 PRODUCT_MANUFACTURER := blu
